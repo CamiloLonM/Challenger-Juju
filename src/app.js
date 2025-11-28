e64c1445e2b4efc2751007563ea84fe053b154fb
CamiloLondonoM <camilo.londono@konecta-group.com>
2025-11-28 13:52:31 -0500

    feat: add app

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+// import swaggerSpec from './utils/swagger.js';
+
+import booksRoutes from './routes/books.js';
+import authRoutes from './routes/auth.js';
+import { errorHandler } from './middlewares/error.js';
+
+const app = express();
+
+const API_VERSION = process.env.API_VERSION || 'v1';
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combined'));
+
+app.use(`/api/${API_VERSION}/auth`, authRoutes);
+app.use(`/api/${API_VERSION}/books`, booksRoutes);
+
+app.use(errorHandler);
+
+// app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+export default app;
